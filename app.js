5e1f1ba55d9519cba243cb55de70e41444b935e9
Noman Maqsood <noman.maqsood91@gmail.com>
2016-01-09 16:13:18 +0500

    Added a Procfile.

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ var routes = require('./routes');
 var models = require('./models');
 var app = express();
 
-app.set('port', process.env.PORT || 3000);
+
 app.use (bodyParser.json ());
 app.use (bodyParser.urlencoded ({ extended: false }));
 
@@ -30,7 +30,7 @@ app.use ("/notifications", routes.Notification);
 
 
 //models.sequelize.sync ().then (function () {
-    app.listen(3000, function() {
-        console.log('Express server listening on port ' + app.get('port'));
-    });
+  app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
 //});
